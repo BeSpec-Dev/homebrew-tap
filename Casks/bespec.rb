49c421d38ff88e57acfb6ab19f590a7ecd6e396f
BeSpec-Dev <35648267+BeSpec-Dev@users.noreply.github.com>
2026-04-12 23:15:51 -0400

    chore: update version to 1.7.0

--- Casks/bespec.rb
+++ Casks/bespec.rb
@@ -2,17 +2,17 @@ cask "bespec" do
   arch arm: "silicon", intel: "intel"
 
   # The exact string of your release tag (without the 'v')
-  version "1.6.3"
+  version "1.7.0"
   
   # PASTE YOUR LINUX HASHES HERE
   if Hardware::CPU.intel?
-    sha256 "2b29ef608b1c91659ec54d3ed4143e0e11b5e70042139cb1c214fb5a55f6e9d5"
+    sha256 "a3ed60f3f1274c83fa15ab88cc4e4f06ef255d646fdc22a4677ce4dc835e267e"
   else
-    sha256 "e3fcd34e2e3b497e5cf5ece0c9e33b0b9e2726608cb58c82f98cb3c191c7ecc9"
+    sha256 "6e5b074c80d803d5752c4a257fc9e8f2d4cd55c10dc06c46003555540ec8c98c"
   end
 
   # This URL builder assumes your GitHub release artifacts follow the pattern:
-  # bespec-v1.6.3-rc1-macos-silicon.dmg
+  # bespec-v1.7.0-macos-silicon.dmg
   url "https://github.com/bespec-dev/bespec/releases/download/v#{version}/bespec-v#{version}-macos-#{arch}.dmg"
   
   name "BeSpec"
